e923dfa532f9491839be5b9353f74300b64fa6d9
SonicGamerEpisode55 <126160675+SonicGamerEpisode55@users.noreply.github.com>
2023-12-08 20:07:52 +0000

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+echo Sonic Mania for Fedora Silverblue - setup
+echo  
+echo Creating container
+distrobox create --image archlinux:latest --name SonicMania --home ~/.mania --additional-packages "base-devel glew glfw libtheora portaudio git cmake"
+cd ~
+mkdir .mania
+cd .mania
+rm .bashrc
+curl https://raw.githubusercontent.com/SonicGamerEpisode55/Sonic-Mania-Fedora-Silverblue/main/first.bashrc > .bashrc
+cd ..
+distrobox enter SonicMania
